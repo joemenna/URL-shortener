b7f2140d0b261d2875afd5974bb0b0da4eecba64
Joe Menna <jomenna1@gmail.com>
2021-01-04 23:18:09 -0800

    Revert "hello"
    This reverts commit 7313274aa240c2dcfa06ccbfc37b671379abcdc2.

--- URL Short.py
+++ URL Short.py
@@ -35,5 +35,3 @@ if shorten_res.status_code == 200:
     # if response is OK, get the shortened URL
     link = shorten_res.json().get("link")
     print("Shortened URL:", link)
-
-    print('Hello')
